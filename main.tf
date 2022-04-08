1b753864eb2868a5cfdd82807ebd41399ba88f45
Alex Ringberg <45809162+alexringberg@users.noreply.github.com>
2022-04-07 21:39:53 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,26 +19,8 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-resource "aws_s3_bucket" "terraform-state" {
-  bucket = "terraform-state-management-ringberg"
-  lifecycle {
-    prevent_destroy = true
-  }
-  versioning {
-    enabled = true
-  }
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-}
-
 resource "aws_s3_bucket_public_access_block" "block" {
-  bucket = aws_s3_bucket.terraform-state.id
+  bucket = "terraform-state-management-ringberg"
 
   block_public_acls       = true
   block_public_policy     = true
