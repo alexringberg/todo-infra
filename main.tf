cd57be72f4a6c611ccc726d9557fe6dda5094cb5
Alex <alex.ringberg@gmail.com>
2022-04-14 00:30:25 -0500

    updating zone id

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ resource "aws_acm_certificate" "default" {
 }
 
 resource "aws_route53_record" "validation" {
-  zone_id = aws_route53_zone.public_zone.zone_id
+  zone_id = "Z047688434K2RNVMN4AG8"
   name    = aws_acm_certificate.default.domain_validation_options.0.resource_record_name
   type    = aws_acm_certificate.default.domain_validation_options.0.resource_record_type
   records = ["${aws_acm_certificate.default.domain_validation_options.0.resource_record_value}"]
