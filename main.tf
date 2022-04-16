c7ebc69b5eb0a692eafd0a21476a0be44d43f96b
Alex Ringberg <45809162+alexringberg@users.noreply.github.com>
2022-04-16 12:38:02 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -34,14 +34,14 @@ provider "aws" {
 #  }
 #}
 
-resource "aws_s3_bucket_public_access_block" "block" {
-  bucket = aws_s3_bucket.terraform-state.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
+#resource "aws_s3_bucket_public_access_block" "block" {
+#  bucket = aws_s3_bucket.terraform-state.id
+#
+#  block_public_acls       = true
+#  block_public_policy     = true
+#  ignore_public_acls      = true
+#  restrict_public_buckets = true
+#}
 
 resource "aws_instance" "todo-app-server" {
   ami                         = "ami-0c02fb55956c7d316"
